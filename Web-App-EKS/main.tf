11769beff38f3c8ca2d7a06a9cc3205b1a198dc8
Hayder Alobaidi <taos_halobaidi@contractor.indeed.com>
2023-08-22 20:32:12 -0700

    update

--- Web-App-EKS/main.tf
+++ Web-App-EKS/main.tf
@@ -1,10 +1,8 @@
-# main.tf
-
 module "vpc" {
-  source = "./vpc"  # Path to the VPC module directory
+  source = "../modules/vpc"  # Path to the VPC module directory
 
-  PROJECT_NAME = var.PROJECT_NAME
-  VPC_CIDR = var.VPC_CIDR
+  PROJECT_NAME          = var.PROJECT_NAME
+  VPC_CIDR              = var.VPC_CIDR
   PUBLIC_SUBNET_CIDR_1A = var.PUBLIC_SUBNET_CIDR_1A
   PUBLIC_SUBNET_CIDR_2B = var.PUBLIC_SUBNET_CIDR_2B
   PRIVATE_SUBNET_CIDR_3A = var.PRIVATE_SUBNET_CIDR_3A
@@ -12,21 +10,23 @@ module "vpc" {
 }
 
 module "internet_gateway" {
-  source = "./internet_gateway"  # Path to the Internet Gateway module directory
+  source = "../modules/nat-gateways"  # Path to the Internet Gateway module directory
 
-  VPC_ID = module.vpc.vpc_id
+  VPC_ID       = module.vpc.vpc_id
+  PROJECT_NAME = var.PROJECT_NAME
+}
+
+module "IAM" {
+  source = "../modules/IAM"  # Path to the IAM Role module directory
+
   PROJECT_NAME = var.PROJECT_NAME
 }
 
 module "EKS" {
-  source = "./nat_gateway_routing"  # Path to the NAT Gateway and Routing module directory
-
-  VPC_ID = module.vpc.vpc_id
+  source       = "../modules/EKS"  # Path to the EKS module directory
   PROJECT_NAME = var.PROJECT_NAME
 }
 
-module "iam_role" {
-  source = "./iam_role"  # Path to the IAM Role module directory
-
-  PROJECT_NAME = var.PROJECT_NAME
+module "NODE_GROUP" {
+  source           = "../modules/EKS-Node-groups"  # Path to the Node Group module directory
 }
--- Web-App-EKS/variables.tf
+++ Web-App-EKS/variables.tf
@@ -1,7 +1,5 @@
-REGION       = "us-east-1"
-PROJECT_NAME = "Web-App-EKS"
+variable "REGION" {}
+variable "PROJECT_NAME" {}
 
 # use data source to get all avalablility zones in region
 data "aws_availability_zones" "available_zones" {}
-
-
