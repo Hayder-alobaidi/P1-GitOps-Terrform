f297330a53915af88087c555828613ca25c1e920
Hayder-alobaidi <93683931+Hayder-alobaidi@users.noreply.github.com>
2023-08-24 11:53:00 -0700

    Create variables.tf

---
+++ modules/nat-gateways/variables.tf
@@ -0,0 +1,7 @@
+variable "VPC_ID" {}
+variable "internet_gateway_id" {}
+
+variable "PUBLIC_SUBNET_1A_ID" {}
+variable "PUBLIC_SUBNET_2B_ID" {}
+variable "PRIVATE_SUBNET_3A_ID" {}
+variable "PRIVATE_SUBNET_4B_ID" {}
